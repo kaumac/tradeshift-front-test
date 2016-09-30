8df6cca74580763f31dc9e9ed4dc885ba55e5ece
Kaue Miyauti Machado <kaumac@gmail.com>
2016-09-30 16:54:46 -0300

    small refactoring to support checking if triangle sides are valid

--- src/triangle.js
+++ src/triangle.js
@@ -1,41 +1,57 @@
 module.exports = {
   getType : function(triangleSides) {
-    if(this.isValid(triangleSides)) {
+    if(this.hasValidFormat(triangleSides) && this.hasValidSides(triangleSides)) {
       const differentSidesMap = {
         1: 'equilateral',
         2: 'isoceles',
         3: 'scalene'
       }
-
-      /*
-        ES6 Provides us with Set's, which has a size method.
-        Using this allows us to check the count os different values in a collection without resorting to "for loops"
-      */
+      /* ES6 Provides us with Set's, which has a size method.
+         Using this allows us to check the count os different values in a collection without resorting to "for loops" */
       const differentSidesSize = (new Set(triangleSides)).size
-
       // return the type of the triangle based on the count of different sides
-      return differentSidesMap[differentSidesSize];
+      return differentSidesMap[differentSidesSize]
+    } else {
+      return 'invalid triangle';
     }
   },
-  isValid: function(triangleSides) {
+  hasValidFormat: function(triangleSides) {
     // triangleSides parameter must be an array
-    if(!triangleSides instanceof Array) {
+    if(triangleSides instanceof Array == false) {
+      console.error('triangle sides must be an array with 3 items')
       return false
     }
     // triangleSides must have 3 sides
     if(triangleSides.length !== 3) {
+      console.error('a triangle must have 3 sides')
       return false
     }
-    // All sides must be numbers
-    // Using filter instead of a "for loop" to check if any of the values are not a number
+    // All sides must be numbers, Using filter instead of a "for loop" to check if any of the values are not a number
     if(triangleSides.filter(side => typeof parseFloat(side) !== 'number').length > 0) {
-      return false
-    }
-    // All sides must be bigger than 0
-    if(triangleSides.filter(side => parseFloat(side) <= 0).length > 0) {
+      console.error('all triangle sides must be numbers');
       return false
     }
 
-    return true;
+    // If all conditions are met, then the parameter format is valid
+    return true
+  },
+  hasValidSides: function(triangleSides) {
+    // Store array items in variables for readability purposes
+    const sideA = parseFloat(triangleSides[0]),
+          sideB = parseFloat(triangleSides[1]),
+          sideC = parseFloat(triangleSides[2])
+
+    // All sides of a triangle must be bigger than 0
+    if(triangleSides.filter(side => parseFloat(side) <= 0).length > 0) {
+      console.error('a triangle side must be bigger than 0');
+      return false
+    }
+    // The sum of 2 triangle sides must always be bigger than one side
+    if((sideA + sideB) <= sideC || (sideB + sideC) <= sideA || (sideC + sideA) <= sideB) {
+      console.error('invalid triangle sides');
+      return false
+    }
+
+    return true
   }
 }
--- tests/triangle.test.js
+++ tests/triangle.test.js
@@ -2,16 +2,22 @@ const expect = require('chai').expect;
 const triangle = require('../src/triangle');
 
 describe('triangle', function() {
-  it('should check for a valid parameter', function() {
-    expect(triangle.isValid(['10', '10', '10'])).to.be.ok;
-    expect(triangle.isValid([20, 30, 40])).to.be.ok;
-    expect(triangle.isValid([20])).to.not.be.ok;
-    expect(triangle.isValid([0, 10, 20])).to.not.be.ok;
-    expect(triangle.isValid('10,10,10')).to.not.be.ok;
+  it('should check for a valid parameter format', function() {
+    expect(triangle.hasValidFormat(['10', '10', '10'])).to.be.ok; // Numbers provided as strings will be converted
+    expect(triangle.hasValidFormat([20, 30, 40])).to.be.ok;
+    expect(triangle.hasValidFormat([20])).to.not.be.ok;
+    expect(triangle.hasValidFormat('10,10,10')).to.not.be.ok;
+  });
+  it('should check for valid parameter values', function() {
+    expect(triangle.hasValidSides(['11', '11', '11'])).to.be.ok;
+    expect(triangle.hasValidSides([18, 8, 18])).to.be.ok;
+    expect(triangle.hasValidSides([10, 18, 12])).to.be.ok;
+    expect(triangle.hasValidSides([0, 13, 13])).to.not.be.ok;
+    expect(triangle.hasValidSides([5, 8, 3])).to.not.be.ok;
   });
   it('should return the triangle type', function() {
     expect(triangle.getType([20,20,20])).to.eql('equilateral');
-    expect(triangle.getType([10,20,10])).to.eql('isoceles');
-    expect(triangle.getType([10,20,30])).to.eql('scalene');
+    expect(triangle.getType([20,10,20])).to.eql('isoceles');
+    expect(triangle.getType([13,16,11])).to.eql('scalene');
   });
 });
